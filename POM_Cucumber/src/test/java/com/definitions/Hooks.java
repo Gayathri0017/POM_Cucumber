86e46bb7158980de8683298358ce530df6cdb4fc
Gayathri0017 <gayathri4866@gmail.com>
2025-04-04 09:55:15 +0530

    update

--- POM_Cucumber/src/test/java/com/definitions/Hooks.java
+++ POM_Cucumber/src/test/java/com/definitions/Hooks.java
@@ -4,12 +4,12 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 public class Hooks{
-@Before
-public static void setUp() {
-	HelperClass.setDriver();
-}
-@After
-public static void tearDown(Scenario s) {
-	HelperClass.tearDown();
-}
+	@Before
+	public static void setUp() {
+		HelperClass.setDriver();
+	}
+	@After
+	public static void tearDown(Scenario s) {
+		HelperClass.tearDown();
+	}
 }
